d10de01fb309da5895570744c75528c4df1e878b
Thurtwings <thurtwings@gmail.com>
2023-02-06 21:36:05 +0100

    Update mateObj.php

--- Devoir PHP/Pages/mateObj.php
+++ Devoir PHP/Pages/mateObj.php
@@ -10,100 +10,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+    <body>
 
-                
-    <div class="container">
-        <div class="row">
-            <div class="row ">
-                <div class="col-12 text-center">
-                    <button id="pair_snakes" class="btn btn-primary">Pair Snakes</button>
+                    
+        <div class="container">
+            <div class="row">
+                <div class="row ">
+                    <div class="col-12 text-center">
+                        <button id="pair_snakes" class="btn btn-primary">Pair Snakes</button>
+                </div>
             </div>
-        </div>
-            <div class="col-6">
-                <h2>Males</h2>
-                <?php 
-                    foreach ($obj->SelectAll($sort) as $value) 
-                    {
-                        if($value["snake_gender"] == "Male" && $value["snake_dead"] == 0)
+                <div class="col-6">
+                    <h2>Males</h2>
+                    <?php 
+                        foreach ($obj->SelectAll("specie") as $value) 
                         {
-                ?> 
-                            <div class="form-check">
-                                <input methode="POST" type="radio" class="form-check-input male" name="male_snake_id" value="<?php echo $value["snake_id"]; ?>">
-                                <label class="form-check-label border col-5">
-                                    Name: <?php echo $value["snake_name"]; ?> 
-                                </label>
-                                <label class="form-check-label border col-5">
-                                    Name: <?php echo $value["snake_specie"]; ?> 
-                                </label>
-                            </div>
-                <?php 
-                        }
-                    }
-                ?>
-            </div>
-            <div class="col-6">
-                <h2>Females</h2>
-                <?php 
-                    foreach ($obj->SelectAll($sort) as $value) 
-                    {
-                        if($value["snake_gender"] == "Female" && $value["snake_dead"] == 0)
-                        {
-                ?> 
-                            <div class="form-check">
-                                <input methode="POST" type="radio" class="form-check-input female" name="female_snake_id" value="<?php echo $value["snake_id"]; ?>">
-                                <label class="form-check-label border col-5">
-                                    Name: <?php echo $value["snake_name"]; ?> 
-                                </label>
-                                <label class="form-check-label border col-5">
-                                    Name: <?php echo $value["snake_specie"]; ?> 
-                                </label>
-                            </div>
-                <?php 
-                        }
-                    }
-                ?>
-            </div>
-        </div>
-        
-    </div>
-
-    <script src="js/jquery-3.6.3.min.js"></script>
-
-    <script>
-        $(document).ready(function() 
-        {
-            $('#pair_snakes').click(function() 
-            {
-                var male_snake_id = $('.male:checked').val();
-                var female_snake_id = $('.female:checked').val();
-                console.log(male_snake_id, "   ", female_snake_id);
-
-                if(male_snake_id == undefined || female_snake_id == undefined) 
-                {
-                    alert("Please select a male snake and a female snake.");
-                } 
-                else 
-                {
-                    $.ajax("add_pair.php",
-                        {
-                            method: "POST",
-                            data:
-                            { 
-                                male_snake_id: male_snake_id, 
-                                female_snake_id: female_snake_id
-                                
-                            },
-                            success: function(result) 
+                            if($value["snake_gender"] == "Male" && $value["snake_dead"] == 0)
                             {
-                                console.log(result);
-                                alert("Snakes have been paired successfully.");
+                    ?> 
+                                <div class="form-check">
+                                    <input methode="POST" type="radio" class="form-check-input male" name="male_snake_id" value="<?php echo $value["snake_id"]; ?>">
+                                    <label class="form-check-label border col-5">
+                                        Name: <?php echo $value["snake_name"]; ?> 
+                                    </label>
+                                    <label class="form-check-label border col-5">
+                                        Specie: <?php echo $value["snake_specie"]; ?> 
+                                    </label>
+                                </div>
+                    <?php 
                             }
-                        });
-                }
-            });
-        });
-    </script>
- 
-</body>
+                        }
+                    ?>
+                </div>
+                <div class="col-6">
+                    <h2>Females</h2>
+                    <?php 
+                        foreach ($obj->SelectAll("specie") as $value) 
+                        {
+                            if($value["snake_gender"] == "Female" && $value["snake_dead"] == 0)
+                            {
+                    ?> 
+                                <div class="form-check">
+                                    <input methode="POST" type="radio" class="form-check-input female" name="female_snake_id" value="<?php echo $value["snake_id"]; ?>">
+                                    <label class="form-check-label border col-5">
+                                        Name: <?php echo $value["snake_name"]; ?> 
+                                    </label>
+                                    <label class="form-check-label border col-5">
+                                        Name: <?php echo $value["snake_specie"]; ?> 
+                                    </label>
+                                </div>
+                    <?php 
+                            }
+                        }
+                    ?>
+                </div>
+            </div>
+            
+        </div> 
+    </body>
 </html>
